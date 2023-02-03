8fa7c4cf4dd8a2dee9fa595681543aa887f38606
ernazaresanbekov <ernazar.03@icloud.com>
2023-02-03 16:43:49 +0600

    PracticeWork11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Person person = new Person();
+        person.firstName = "Shabdan";
+        person.lastName = "Tilek uulu";
+        person.age = 26;
+        person.gender = "male";
+        person.countryOfBirth = "Kyrgyzstan";
+        System.out.println(person.work("Boris"));
+        System.out.println(person.eat("Salat"));
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Person {
-}
+public class Person {
+
+        String firstName;
+        String lastName;
+        int age;
+        String gender;
+        String countryOfBirth;
+
+        public String work(String work) {
+            return "first name "+firstName +" "+"last name "+ lastName +" " + "contry of birth "+ work;
+        }public String eat (String eat){
+            return "eat "+eat;
+
+        }
+    }
+
